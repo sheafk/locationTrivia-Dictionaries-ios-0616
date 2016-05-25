f1e45dd4a87fe53ed7417cebb3c800f24dc2f09a
sheafk <sheafureyking@gmail.com>
2016-05-25 18:05:44 -0400

    Done.

--- locationTrivia-Dictionaries/FISAppDelegate.h
+++ locationTrivia-Dictionaries/FISAppDelegate.h
@@ -6,10 +6,19 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- *  Declare your method names here.
- 
- */
+- (NSString *)stringByTruncatingNameOfLocation: (NSDictionary *)location
+                                      toLength: (NSUInteger)length;
+
+- (NSDictionary *)dictionaryForLocationWithName: (NSString *)name
+                                       latitude: (CGFloat)latitude
+                                      longitude: (CGFloat)longitude;
+
+- (NSArray *)namesOfLocations: (NSArray *)locations;
+
+- (BOOL)dictionaryIsValidLocation: (NSDictionary *)dictionary;
+
+- (NSDictionary *)locationNamed: (NSString *)name
+                    inLocations: (NSArray *)locations;
+
 
 @end
--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,65 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+
+    NSMutableArray *hold = [[NSMutableArray alloc]init];
+    
+    if ([location[@"name"] length] > length) {
+    NSString *string = [location[@"name"] substringToIndex:length];
+    NSLog(@"What is the string: %@", string);
+    NSLog(@"Let's check the length: %lu", length);
+        [hold addObject:string];
+    }
+    else {
+        NSString *string = location[@"name"];
+        [hold addObject:string];
+    }
+    
+    NSString *outsideString = [hold firstObject];
+    
+    return outsideString;
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    
+    NSDictionary *newDict = [[NSDictionary alloc] initWithObjectsAndKeys:name, @"name", @(latitude), @"latitude", @(longitude), @"longitude", nil];
+    
+    //NSLog(@"Look at dictionary: %@", newDict);
+    return newDict;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    
+   NSArray *locationsForKey = [locations valueForKey:@"name"];
+    //NSLog(@"Look at the arrya: %@", locationsForKey);
+    return locationsForKey;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    
+   // NSArray *allKeys = [dictionary allKeys];
+  //  NSUInteger b = [allKeys count];
+   
+    //NSLog(@"Check dictionary keys: %@", allKeys);
+   // NSLog(@"Number of keys: %lu", b);
+    
+    BOOL answer = ([[dictionary allKeys] containsObject:@"name" ] && [[dictionary allKeys] containsObject:@"latitude"] && [[dictionary allKeys] containsObject:@"longitude"] && ([[dictionary allKeys] count] == 3));
+    
+    return answer;
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+
+    //NSLog(@"Locations: %@", locations);
+    
+    for (NSDictionary *dictionary in locations) {
+        NSString *foundLocation = dictionary[@"name"];
+    if ([foundLocation isEqualToString:name]) {
+        return dictionary;
+        }
+    }
+    return nil;
+}
 
 @end
